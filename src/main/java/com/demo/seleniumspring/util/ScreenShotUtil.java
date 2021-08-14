215cfffb0abebb9cfbdbaa928d8d2ffd34bc090a
Anish Sebastian <anishst@hotmail.com>
2021-08-13 23:09:17 -0400

    fix applied for failed screenshot attemp

--- src/main/java/com/demo/seleniumspring/util/ScreenShotUtil.java
+++ src/main/java/com/demo/seleniumspring/util/ScreenShotUtil.java
@@ -1,5 +1,6 @@
 package com.demo.seleniumspring.util;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -26,8 +27,14 @@ public class ScreenShotUtil {
 
     public void takeScreenShot(final String imgName) throws IOException {
         // takes screenshot as saves to path in app properties file using given imgName ex. test.png
-        File sourceFile = this.driver.getScreenshotAs(OutputType.FILE);
-        FileCopyUtils.copy(sourceFile, this.path.resolve(imgName).toFile());
+        try {
+            File sourceFile = this.driver.getScreenshotAs(OutputType.FILE);
+            FileCopyUtils.copy(sourceFile, this.path.resolve(imgName).toFile());
+            System.out.println("Saving screenshot to " + path);
+        } catch (Exception e){
+            System.out.println("Something went wrong with screenshot capture" + e);
+        }
+
 
     }
 }
