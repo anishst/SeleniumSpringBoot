5c665ff414163c8e0ddbe6681f71bfb1f99c30b4
Anish Sebastian <anishst@hotmail.com>
2021-08-13 21:23:28 -0400

    testing chrome headless

--- src/main/java/com/demo/seleniumspring/config/WebDriverConfig.java
+++ src/main/java/com/demo/seleniumspring/config/WebDriverConfig.java
@@ -3,6 +3,7 @@ package com.demo.seleniumspring.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +19,10 @@ public class WebDriverConfig {
     public WebDriver chromeDriver() {
         // this is the bean class for chrome driver
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        return new ChromeDriver(options=options);
     }
 
     @Bean
